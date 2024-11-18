2e205db667e6ffe37f8d0e92222ce5fa38c0fd82
BIllCIfhEr <bill26122546rock26@gmail.com>
2024-11-18 18:11:29 +0000

    helppppppppppppp

--- test_calculator.py
+++ test_calculator.py
@@ -7,7 +7,41 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(5, 6), 11)
+    def test_add2(self):
+        self.assertEqual(self.calc.add(6, -9), -3)
+    def test_add3(self):
+        self.assertEqual(self.calc.add(-3, -8), -11)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(6, 2), 4)
+    def test_subtract2(self):
+        self.assertEqual(self.calc.subtract(8, -7), 15)
+    def test_subtract3(self):
+        self.assertEqual(self.calc.subtract(-7, -9), 2)
+        
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(8, 3), 24)
+    def test_multiply2(self):
+        self.assertEqual(self.calc.multiply(6, -3), -18)
+    def test_multiply3(self):
+        self.assertEqual(self.calc.multiply(-3, -10), 30)
+        
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(9, 3), 3)
+    def test_divide2(self):
+         self.assertEqual(self.calc.divide(16, -8), -2)
+    def test_divide3(self):
+        self.assertEqual(self.calc.divide(-7, -7), 1)
+    def test_divide4(self):
+        self.assertEqual(self.calc.divide(81, 9), 9)
+    
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10, -4), -2)
+    def test_modulo2(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+    def test_modulo3(self):
+        self.assertEqual(self.calc.modulo(10, 6), 4)
 
     # Add the following test methods to the TestCalculator class:
 
